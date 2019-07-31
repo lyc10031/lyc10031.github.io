5a56faf4224b2dd1d228a4e42af4d3f97eb7f366
lyc10031 <lyc10031@126.com>
2019-07-31 15:18:53 +0800

    add all file

---
+++ _site/assets/search.js
@@ -0,0 +1,1 @@
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"在使用golang时常见的错误",'url':"/2019/07/31/go-blog2.html"},{'title':"Golang 数据类型的转换",'url':"/2019/07/31/Go1.html"}]};
